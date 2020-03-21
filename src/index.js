59b3258fc1809bd82913abe26ce789f9d8c1a828
NikitaMigushev <migushev@gmail.com>
2020-03-21 22:21:40 +0300

    init: start human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function toReadable (number) {
-  
+const units = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninenty'];
+const hundred = 'hundred';
+
+
+module.exports = function toReadable(number) {
+    let numberToString = number.toString().split('');
+    let result = `${units[numberToString[0] - 1]} ${hundred} ${tens[numberToString[1] - 1]} ${units[numberToString[2] - 1]}`
+    return result;
 }
